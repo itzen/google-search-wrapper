c99c55342094b42f77b24f18ad137621f40ba9a9
Vaidas <bagdonas.vaidas@gmail.com>
2015-09-29 13:18:14 +0300

    init curl class

--- src/GoogleKeywordPosition.php
+++ src/GoogleKeywordPosition.php
@@ -2,7 +2,13 @@
 
 namespace GoogleKeywordPosition;
 
+use Curl\Curl;
+
 class GoogleKeywordPosition
 {
+    private $curl;
 
+    public function __construct(){
+        $this->curl = new Curl();
+    }
 }
